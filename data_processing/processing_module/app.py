8865c35bc2f0410f7d8cd4057183297a668964da
Dmitry Yatsenko <Dmitry.Yatsenko@gmail.com>
2020-12-27 23:40:31 +0200

    Add configurable logging

--- data_processing/processing_module/app.py
+++ data_processing/processing_module/app.py
@@ -1,6 +1,10 @@
 import os
+import sys
+import logging
+
 from datetime import timedelta, datetime
 import faust
+
 from data_model import Signal
 from shared_functions.resampling import mean
 
@@ -9,7 +13,9 @@ import random
 INPUT_TOPIC_NAME = os.environ["INPUT_TOPIC_NAME"]
 OUTPUT_TOPIC_NAME = os.environ["OUTPUT_TOPIC_NAME"]
 WINDOW_SIZE = int(os.environ["WINDOW_SIZE"])  # seconds
-WINDOW_TTL = 10  # seconds
+WINDOW_TTL = 1  # seconds
+
+logging.basicConfig(stream=sys.stdout, level=getattr(logging, os.environ["LOG_LEVEL"].upper(), logging.INFO))
 
 app = faust.App('processing', broker=f"kafka://{os.environ['KAFKA_BOOTSTRAP_NODES']}", topic_partitions=1)
 
@@ -24,20 +30,20 @@ def window_processor(key, signals):
     values = [signal.value for signal in signals]
     result = mean(values)
 
-    print(f"DEBUG aggregation result: {sensor_id}:{result}, ts:{ts}")
+    logging.debug(f"Aggregation result: {sensor_id}:{result}, ts:{ts}")
     output_topic.send_soon(key=sensor_id, value=Signal(ts=ts, sensor_id=sensor_id, value=result))
 
 
 signals_table = (app.Table("signals", default=list, on_window_close=window_processor, partitions=1)
                  .tumbling(timedelta(seconds=WINDOW_SIZE), expires=timedelta(seconds=WINDOW_TTL))
-                 #.relative_to_field(Signal.ts)
+                 .relative_to_field(Signal.ts)
                  )
 
 
 @app.agent(input_topic)
 async def process(stream):
     async for signal in stream.group_by(Signal.sensor_id):
-        print(f"DEBUG received signal: {signal}")
+        logging.debug(f"Received signal: {signal}")
 
 
 @app.agent(input_topic)
@@ -48,16 +54,16 @@ async def collect_signals(stream):
         signals_table[s.sensor_id] = value_list
 
 
-@app.timer(1)
-async def produce():
-    sensor_id = "test_sensor"
-    await input_topic.send(
-        key=sensor_id,
-        value=Signal(
-            ts=datetime.now(),
-            sensor_id=sensor_id,
-            value=89)
-    )
+# @app.timer(1)
+# async def produce():
+#     sensor_id = "test_sensor"
+#     await input_topic.send(
+#         key=sensor_id,
+#         value=Signal(
+#             ts=datetime.now(),
+#             sensor_id=sensor_id,
+#             value=89)
+#     )
 
 
 @app.timer(1)
--- data_processing/processing_module/data_model.py
+++ data_processing/processing_module/data_model.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from faust import Record
 
 
-class Signal(Record):
+class Signal(Record, isodates=True, serializer='json'):
     ts: datetime
     sensor_id: str
     value: float
